7f6cc9f11e552daabdb89573f58732981a2eb58f
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2024-09-09 20:01:31 +0530

    Argurment

--- 05_Arguments[0]().js
+++ 05_Arguments[0]().js
@@ -10,11 +10,11 @@ const object = {
 };
 object.method(callback, 1, 2);
 
-//-------------------------------------
+//---------------------------------------------------------------------------------------------------------------
 
 var length = 4;
 function callback() {
-  console.log(this.length); // What is logged? Answer is 4
+  console.log(this.length); // What is logged? Answer is : 4
 }
 const object = {
   length: 5,
@@ -23,3 +23,13 @@ const object = {
   }
 };
 object.method(callback, 1, 2);
+
+/*
+Key Concepts:
+this inside arguments[0]():
+  When arguments[0]() is invoked, the this keyword inside the callback function refers to the arguments object because
+  of how the function is called.
+arguments.length:
+  The arguments object has a length property that represents the number of arguments passed to the function. In this case,
+  it's 3 because method() was called with three arguments: callback, 1, and 2.
+*/
