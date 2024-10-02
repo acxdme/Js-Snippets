59c79f1a50a0fe669ebcc06fc352b2c94b597d11
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2024-10-02 20:43:23 +0530

    flatten a nested array

--- 03_FlattenANestedArray.js
+++ 03_FlattenANestedArray.js
@@ -17,3 +17,26 @@ depth = 2;
 flatten(arr,depth);
 
 console.log(res);
+
+// flatten a nested array : alternative approach 
+
+//flattening a nested array
+
+let arr = [1,2,3,[4,5,[6,7]]];
+
+function flattenArray(arr){
+let result = [];
+    
+    for(let i =0 ; i < arr.length ; i++){
+        if(Array.isArray(arr[i])){
+           result = result.concat(flattenArray(arr[i])) ;
+        }
+        else{
+         result.push(arr[i]);
+        }
+    }
+    
+    return result;
+}
+const flattenRes = flattenArray(arr);
+console.log(flattenRes);
