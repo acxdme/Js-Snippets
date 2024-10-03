9548ab441f1839d6be0ef67f0ca552a37d5b8617
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2024-10-03 22:30:55 +0530

    Nested currying using value

--- 12_NestedCurrying.js
+++ 12_NestedCurrying.js
@@ -10,3 +10,22 @@ function sum(a) {
 }
 
 console.log(sum(9)(2)(3)(4)(5)());
+//----------------------------------------------------------------
+//using valueOf operator
+function sum(a) {
+  let currentSum = a;
+
+  function innerSum(b) {
+    currentSum += b; 
+    return innerSum;
+  }
+
+  // Overriding `toString` to return the accumulated sum when the function is evaluated as a string
+  innerSum.toString = function() {
+    return currentSum;
+  };
+
+  return innerSum;
+}
+
+console.log(sum(9)(2)(3)(4)(5));  // Outputs: 23
